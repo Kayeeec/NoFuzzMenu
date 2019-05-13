22306e51ef3ea0df907156952570825ec826bb38
Barbora Kompišová <barbora.kompisova@gmail.com>
2019-05-13 13:58:47 +0200

    start adding maps

--- app/src/main/java/cz/muni/fi/nofuzzmenu/activity/MainActivity.kt
+++ app/src/main/java/cz/muni/fi/nofuzzmenu/activity/MainActivity.kt
@@ -6,18 +6,29 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import cz.muni.fi.nofuzzmenu.R
 import cz.muni.fi.nofuzzmenu.fragment.RestaurantListFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ActivityCompat
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import cz.muni.fi.nofuzzmenu.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 10
+    }
+
+    var locationPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+
+        getLocationPermission()
         /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -60,5 +71,43 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun getLocationPermission() {
+        /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        // TODO updateLocationUI()
     }
 }
