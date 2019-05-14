796402920c1244fcda7545fb69c9404cde50eea7
Barbora Kompišová <barbora.kompisova@gmail.com>
2019-05-14 17:02:05 +0200

    Use Activity for location pick, use current location

--- app/src/main/java/cz/muni/fi/nofuzzmenu/activity/LocationPickActivity.kt
+++ app/src/main/java/cz/muni/fi/nofuzzmenu/activity/LocationPickActivity.kt
@@ -1,35 +1,55 @@
 package cz.muni.fi.nofuzzmenu.activity
 
+import android.Manifest
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import cz.muni.fi.nofuzzmenu.R
+import android.location.LocationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AlertDialog
+import android.location.LocationListener
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
 
 class LocationPickActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    companion object {
+        const val LOCATION_PERMISSION = 10
+        const val TAG = "Location pick activiry"
+    }
+
     private lateinit var mMap: GoogleMap
+    private val manager = getSystemService( Context.LOCATION_SERVICE ) as LocationManager
+    var currentLocation: Location = Location(LocationManager.GPS_PROVIDER)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_pick)
+
+        // check if GPS is on
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps()
+        }
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           requestLocationPermission()
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        // use google's helpers for device location
-        // Construct a PlaceDetectionClient.
-        //mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
-
-        // Construct a FusedLocationProviderClient.
-        // mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
+        // TODO: save lat, lon to shared Prefs on marker select?
     }
 
     /**
@@ -43,10 +63,101 @@ class LocationPickActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        // clear markers
+        mMap.clear()
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val startingLocation = LatLng(currentLocation.latitude, currentLocation.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(startingLocation))
     }
+
+    private fun buildAlertMessageNoGps() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+            .setCancelable(false)
+            .setPositiveButton("Yes")
+                { _, _ -> startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
+            .setNegativeButton("No") { dialog, _ -> dialog.cancel() }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation = location
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            // NO-OP
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+            //NO-OP
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+            //NO-OP
+        }
+    }
+
+    private fun requestLocationPermission() {
+        // Permission is not granted
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            showLocationPermissionRationale(this)
+        } else {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_PERMISSION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // can work with location now
+                    Log.i(TAG, "Location permission granted")
+                    if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        currentLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5f, locationListener)
+                    } else {
+                        setDefaultLocation()
+                    }
+                } else {
+                    // current location unavailable, use default
+                    Log.d("location pick", "Location permission not granted, using default of 0,0")
+                    setDefaultLocation()
+                }
+            }
+        }
+    }
+
+    private fun setDefaultLocation() {
+        currentLocation.latitude = 0.0
+        currentLocation.longitude = 0.0
+    }
+
+    private fun showLocationPermissionRationale(context: Context) {
+        val alertBuilder = AlertDialog.Builder(context)
+        alertBuilder.setCancelable(true)
+        alertBuilder.setTitle("Location permission request")
+        alertBuilder.setMessage("The app automatically searches for restaurants around you. Without your location" +
+                "we can not suggest relevant restaurants.")
+        alertBuilder.setPositiveButton(android.R.string.ok) { dialog, which ->
+            // go to settings page to set the permission
+            val intent = Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", packageName, null))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+        val alert = alertBuilder.create()
+        alert.show()
+    }
+
 }
