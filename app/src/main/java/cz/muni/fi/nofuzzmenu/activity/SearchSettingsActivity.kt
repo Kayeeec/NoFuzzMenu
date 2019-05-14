24c3643dd8224fee1df6612c1eaa7089b584ddc6
Barbora Kompišová <barbora.kompisova@gmail.com>
2019-05-14 17:02:05 +0200

    Use Activity for location pick, use current location

--- app/src/main/java/cz/muni/fi/nofuzzmenu/activity/SearchSettingsActivity.kt
+++ app/src/main/java/cz/muni/fi/nofuzzmenu/activity/SearchSettingsActivity.kt
@@ -3,7 +3,6 @@ package cz.muni.fi.nofuzzmenu.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cz.muni.fi.nofuzzmenu.R
-import cz.muni.fi.nofuzzmenu.fragment.MapFragment
 import cz.muni.fi.nofuzzmenu.fragment.SearchSettingsFragment
 
 class SearchSettingsActivity : AppCompatActivity() {
@@ -12,9 +11,6 @@ class SearchSettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_settings)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.location_settings, MapFragment())
-            .commit()
         supportFragmentManager.beginTransaction()
             .replace(R.id.search_settings, SearchSettingsFragment())
             .commit()
